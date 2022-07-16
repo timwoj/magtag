cc5f9884395c04c845c9a3115f05a7175771e3d0
Tim Wojtulewicz <timwoj@gmail.com>
2022-07-15 17:05:06 -0700

    Let set_text() call refresh instead of doing it manually

--- code.py
+++ code.py
@@ -2,7 +2,6 @@ import time
 import board
 import alarm
 import json
-import sys
 
 from adafruit_display_shapes.rect import Rect
 from adafruit_magtag.magtag import MagTag
@@ -16,19 +15,6 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-def try_refresh(magtag):
-    """Attempt to refresh the display. Catch 'refresh too soon' error
-    and retry after waiting 10 seconds.
-    """
-    try:
-        magtag.refresh()
-    except RuntimeError as too_soon_error:
-        # catch refresh too soon
-        print(too_soon_error)
-        print("waiting before retry refresh()")
-        time.sleep(10)
-        magtag.refresh()
-
 # How frequently we should automatically update the screen in milliseconds
 TIME_BETWEEN_REFRESHES = 15 * 60 * 1000
 
@@ -41,7 +27,6 @@ magtag.add_text(
     text_position=(10, (magtag.graphics.display.height // 2)-1),
 )
 magtag.set_text(f'Refreshing from HomeAssistant...')
-try_refresh(magtag)
 
 try:
     magtag.network.connect(max_attempts=3)
@@ -50,10 +35,8 @@ except OSError as err:
     magtag.remove_all_text()
     magtag.add_text(
         text_font="/fonts/Arial-Bold-12.pcf",
-        text_position=(10, 15),
-    )
-    magtag.set_text(f'Failed to connect to wifi.\nWill retry again in 30 seconds')
-    try_refresh(magtag)
+        text_position=(10, (magtag.graphics.display.height // 2)-1),
+        text=f'Failed to connect to wifi.\nWill retry again in 30 seconds')
 
     time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic()+30000)
     alarm.exit_and_deep_sleep_until_alarms(time_alarm)
@@ -85,51 +68,58 @@ print(pool_data)
 
 # Clear all of the current text from the screen, which should just
 # be the start-up message.
+print('clearing screen')
 magtag.remove_all_text()
 
+print('adding text')
 # Build up the output screen with all of the data
 magtag.add_text(
     text_font="/fonts/Arial-Bold-12.pcf",
     text_position=(10, 15),
+    is_data=False
 )
-magtag.set_text(f"Pool: {pool_data.get('pool','unknown')}", 0, 0);
-
 magtag.add_text(
     text_font="/fonts/Arial-Bold-12.pcf",
     text_position=(10, 35),
+    is_data=False
 )
-magtag.set_text(f"Air: {pool_data.get('air','unknown')}", 1, 0);
+magtag.add_text(
+    text_font="/fonts/Arial-Bold-12.pcf",
+    text_position=(10, 55),
+    is_data=False
+)
+magtag.add_text(
+    text_font="/fonts/Arial-Bold-12.pcf",
+    text_position=(10, 75),
+    is_data=False
+)
+print('setting text')
 
 if pool_data.get('fountain', False):
     fountain = "On"
 else:
     fountain = "Off"
 
-magtag.add_text(
-    text_font="/fonts/Arial-Bold-12.pcf",
-    text_position=(10, 55),
-)
-magtag.set_text(f"Water Feature: {fountain}", 2, 0);
-
 when = pool_data.get('updated',None)
 if when:
-    updated_at = f'{when.tm_mon}/{when.tm_mday} {when.tm_hour}:{when.tm_min}:{when.tm_sec}'
+#    updated_at = f'{when.tm_mon : 2}/{when.tm_mday} {when.tm_hour}:{when.tm_min}:{when.tm_sec}'
+    updated_at = f'{when.tm_mon:0>2}/{when.tm_mday:0>2} {when.tm_hour:0>2}:{when.tm_min:0>2}:{when.tm_sec:0>2}'
 else:
     updated_at = 'unknown'
-magtag.add_text(
-    text_font="/fonts/Arial-Bold-12.pcf",
-    text_position=(10, 75),
-)
+    
+magtag.set_text(f"Pool: {pool_data.get('pool','unknown')}", 0, False);
+magtag.set_text(f"Air: {pool_data.get('air','unknown')}", 1, False);
+magtag.set_text(f"Water Feature: {fountain}", 2, False);
 magtag.set_text(updated_at, 3, True)
 
-try_refresh(magtag)
-
+print('save data')
 # Store the data in the alarm cache so that it can be used again
 # on the next refresh if we fail to connect to HASS.
 pool_data['updated'] = list(pool_data['updated'])
 data_mem = json.dumps(pool_data).encode('utf-8')
 alarm.sleep_memory[0:len(data_mem)] = data_mem
 
+print('sleeping')
 magtag.peripherals.buttons[0].deinit()
 magtag.peripherals.buttons[1].deinit()
 a_alarm = alarm.pin.PinAlarm(pin=board.BUTTON_A, value=False, pull=True)

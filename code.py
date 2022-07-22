ce4175224f525215b90a442e7e9448ce0b948c46
Tim Wojtulewicz <timwoj@gmail.com>
2022-07-21 22:27:16 -0700

    Add button labels, bit of cleanup

--- code.py
+++ code.py
@@ -27,12 +27,14 @@ TIME_BETWEEN_REFRESHES = 15 * 60 * 1000
 # magtag device instance
 magtag = MagTag()
 
+print(f'building ui {time.monotonic()}')
+
 # Build up the display elements so that they can be turned on and off as we
 # do the actual loading of data
 pool_group = displayio.Group()
-pool_labels = [label.Label(font=LABEL_FONT, text="Label: Value",
+pool_labels = [label.Label(font=LABEL_FONT, text="A",
                            color=0x000000, anchor_point=(0, 0),
-                           hidden=True) for _ in range(3)]
+                           hidden=True) for _ in range(4)]
 
 y_offset = 10
 for lbl in pool_labels:
@@ -40,31 +42,51 @@ for lbl in pool_labels:
     y_offset += 25
     pool_group.append(lbl)
 
-center_label = label.Label(LABEL_FONT, text='Refreshing from HomeAssistant...',
-                           color=0x000000, anchor_point=(0, 0), hidden=True)
-center_label.anchored_position = (10, (magtag.graphics.display.height // 2)-1)
-center_label.hidden = True
+print(f'1 {time.monotonic()}')
 
-date_battery_group = displayio.Group()
 date_label = label.Label(DATE_FONT, text="A", color=0x000000,
-                         anchor_point=(1, 0), hidden=True)
+                         anchor_point=(1, 0))
 date_label.anchored_position = (magtag.graphics.display.width-5, 5)
-date_battery_group.append(date_label)
 
 battery_label = label.Label(DATE_FONT, text="A", color=0x000000,
-                            anchor_point=(1, 0), hidden=True)
-battery_label.anchored_position = (magtag.graphics.display.width-5, 15)
+                            anchor_point=(1, 0))
+battery_label.anchored_position = (magtag.graphics.display.width-5, 17)
+
+date_battery_group = displayio.Group()
+date_battery_group.append(date_label)
 date_battery_group.append(battery_label)
 
+print(f'2 {time.monotonic()}')
+
+button_group = displayio.Group()
+refresh_button_label = label.Label(DATE_FONT, text="Refresh", color=0x000000,
+                                   anchor_point=(0, 1))
+refresh_button_label.anchored_position = (5, magtag.graphics.display.height-5)
+
+fountain_button_label = label.Label(DATE_FONT, text="Fountain", color=0x000000,
+                                    anchor_point=(0, 1))
+fountain_button_label.anchored_position = (75, magtag.graphics.display.height-5)
+
+light_button_label = label.Label(DATE_FONT, text="Light", color=0x000000,
+                                 anchor_point=(0, 1))
+light_button_label.anchored_position = (158, magtag.graphics.display.height-2)
+
+button_group = displayio.Group()
+button_group.append(refresh_button_label)
+button_group.append(fountain_button_label)
+button_group.append(light_button_label)
+print(f'3 {time.monotonic()}')
+
 magtag.splash.append(pool_group)
-magtag.splash.append(center_label)
 magtag.splash.append(date_battery_group)
+magtag.splash.append(button_group)
+
+print(f'built ui elements {time.monotonic()}')
 
 def refresh_display():
     # refresh display
     time.sleep(magtag.display.time_to_refresh + 1)
     magtag.display.refresh()
-    time.sleep(magtag.display.time_to_refresh + 1)
 
 ##### MAIN #####
 
@@ -119,17 +141,24 @@ pool_labels[0].hidden = False
 pool_labels[1].text = f"Air: {pool_data.get('air','unknown')}"
 pool_labels[1].hidden = False
 
+if pool_data.get('light', False):
+    fountain = "On"
+else:
+    fountain = "Off"
+
+pool_labels[2].text = f"Light: {fountain}"
+pool_labels[2].hidden = False
+
 if pool_data.get('fountain', False):
     fountain = "On"
 else:
     fountain = "Off"
 
-pool_labels[2].text = f"Water Feature: {fountain}"
-pool_labels[2].hidden = False
+pool_labels[3].text = f"Water Feature: {fountain}"
+pool_labels[3].hidden = False
 
 when = pool_data.get('updated',None)
 if when:
-#    updated_at = f'{when.tm_mon : 2}/{when.tm_mday} {when.tm_hour}:{when.tm_min}:{when.tm_sec}'
     updated_at = f'{when.tm_mon:0>2}/{when.tm_mday:0>2} {when.tm_hour:0>2}:{when.tm_min:0>2}:{when.tm_sec:0>2}'
 else:
     updated_at = 'unknown'
@@ -137,10 +166,14 @@ else:
 date_label.text = updated_at
 date_label.hidden = False
 
-battery_label.text = f'Battery: {magtag.peripherals.battery}'
+battery_label.text = f'Battery: {magtag.peripherals.battery:.2f} V'
 battery_label.hidden = False
 
+print(f'updated elements {time.monotonic()}')
+
 refresh_display()
+
+print(f'refreshed display {time.monotonic()}')
 
 # Store the data in the alarm cache so that it can be used again
 # on the next refresh if we fail to connect to HASS.
@@ -148,13 +181,22 @@ pool_data['updated'] = list(pool_data['updated'])
 data_mem = json.dumps(pool_data).encode('utf-8')
 alarm.sleep_memory[0:len(data_mem)] = data_mem
 
-print('sleeping')
+print(f'stored old data {time.monotonic()}')
+
+# Deinit all of the buttons since by default the magtag has control
+# over them. You have to deinit them to be able to make alarms
+# using them.
 magtag.peripherals.buttons[0].deinit()
 magtag.peripherals.buttons[1].deinit()
+#magtag.peripherals.buttons[2].deinit()
+#magtag.peripherals.buttons[3].deinit()
+
 a_alarm = alarm.pin.PinAlarm(pin=board.BUTTON_A, value=False, pull=True)
 b_alarm = alarm.pin.PinAlarm(pin=board.BUTTON_B, value=False, pull=True)
 #c_alarm = alarm.pin.PinAlarm(pin=board.BUTTON_C, value=False, pull=True)
 #d_alarm = alarm.pin.PinAlarm(pin=board.BUTTON_D, value=False, pull=True)
 time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic()+TIME_BETWEEN_REFRESHES)
+
+print(f'sleeping {time.monotonic()}')
 
 alarm.exit_and_deep_sleep_until_alarms(a_alarm, b_alarm, time_alarm)#, c_alarm, d_alarm)

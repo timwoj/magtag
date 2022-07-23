f4a33862160d5d16df89994a3649db91ce8d10f0
Tim Wojtulewicz <timwoj@gmail.com>
2022-07-22 22:09:39 -0700

    Redo hass_api to only make single request

--- hass_api.py
+++ hass_api.py
@@ -6,43 +6,66 @@ try:
 except ImportError:
     print('Need to put some secrets in secrets.py')
     raise
-    
+
 hass_api_url = secrets.get('hass_api_url', '')
 if not hass_api_url:
     raise ImportError('Missing hass_api_url in secrets')
 
 # URLs to fetch from
-POOL_TEMPERATURE = f'{hass_api_url}/sensor.pentair_15_a3_34_pool_temperature'
-AIR_TEMPERATURE = f'{hass_api_url}/sensor.pentair_15_a3_34_air_temperature'
-WATER_FEATURE = f'{hass_api_url}/switch.pentair_15_a3_34_water_feature'
-LIGHT_FIXTURE = f'{hass_api_url}/light.pentair_15_a3_34_pool_light'
+STATES_URL = f'{hass_api_url}/states'
+POOL_TEMPERATURE = f'{hass_api_url}/states/sensor.pentair_15_a3_34_pool_temperature'
+AIR_TEMPERATURE = f'{hass_api_url}/states/sensor.pentair_15_a3_34_air_temperature'
+WATER_FEATURE = f'{hass_api_url}/states/switch.pentair_15_a3_34_water_feature'
+LIGHT_FIXTURE = f'{hass_api_url}/states/light.pentair_15_a3_34_pool_light'
+AUTOMATION_URL = f'{hass_api_url}/services/automation/trigger'
 
-request_headers = { "Authorization": f"Bearer {secrets.get('hass_bearer_token','')}" }
+request_headers = {"Authorization": f"Bearer {secrets.get('hass_bearer_token','')}",
+                   "Content-Type": "application/json"}
 
 def get_pool_data(magtag):
     """Retrieve the data from HomeAssistant about what the pool is doing."""
 
+    hass_data = {
+        'pool': 'Unknown',
+        'air': 'Unknown',
+        'fountain': False,
+        'light': False
+    }
+
     try:
-        response = magtag.network.fetch(POOL_TEMPERATURE, headers=request_headers)
-        pool_temp = f"{response.json().get('state', 0)} {response.json().get('attributes', {}).get('unit_of_measurement', '°F')}"
-        print('')
+        response = magtag.network.fetch(STATES_URL, headers=request_headers)
+        data = response.json()
 
-        response = magtag.network.fetch(AIR_TEMPERATURE, headers=request_headers)
-        air_temp = f"{response.json().get('state', 0)} {response.json().get('attributes', {}).get('unit_of_measurement', '°F')}"
-        print('')
+        pool_temp_data = [x for x in data if x['entity_id'] == 'sensor.pentair_15_a3_34_pool_temperature']
+        if pool_temp_data:
+            d = pool_temp_data[0]
+            hass_data['pool'] = f"{d.get('state', 0)} {d.get('attributes', {}).get('unit_of_measurement', '°F')}"
 
-        response = magtag.network.fetch(WATER_FEATURE, headers=request_headers)
-        fountain = f"{response.json().get('state', 'off')}" != "off"
-        print('')
+        air_temp_data = [x for x in data if x['entity_id'] == 'sensor.pentair_15_a3_34_air_temperature']
+        if air_temp_data:
+            d = air_temp_data[0]
+            hass_data['air'] = f"{d.get('state', 0)} {d.get('attributes', {}).get('unit_of_measurement', '°F')}"
+
+        fountain_data = [x for x in data if x['entity_id'] == 'sensor.pentair_15_a3_34_pool_temperature']
+        if fountain_data:
+            d = fountain_data[0]
+            if d.get('state','off') == 'Off':
+                hass_data['fountain'] = 'Off'
+            else:
+                hass_data['fountain'] = 'On'
+
+        light_data = [x for x in data if x['entity_id'] == 'sensor.pentair_15_a3_34_pool_temperature']
+        if light_data:
+            d = light_data[0]
+            if d.get('state','off') == 'Off':
+                hass_data['light'] = 'Off'
+            else:
+                hass_data['light'] = 'On'
 
         now = time.localtime()
 
-        return {
-            'pool': pool_temp,
-            'air': air_temp,
-            'fountain': fountain,
-            'updated': now
-        }
+        hass_data['updated'] = time.localtime()
+        return hass_data
 
     except Exception as err:
 
